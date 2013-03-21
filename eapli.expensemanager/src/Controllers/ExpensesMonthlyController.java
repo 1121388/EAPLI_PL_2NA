7241aacdbe29abedcd406db6d3fad2c455a73ad9
Adao Santos <adaoss@gmail.com>
2013-03-21 20:01:30 +0000

    Colocação dos metodos para consultar gastos mensais

---
+++ eapli.expensemanager/src/Controllers/ExpensesMonthlyController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controllers;
+
+/**
+ *
+ * @author adao
+ */
+public class ExpensesMonthlyController {
+    public void consultaDadosMensais(String mes, String ano) {
+    }
+}
---
+++ eapli.expensemanager/src/Presentation/ExpensesMonthlyUI.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Presentation;
+
+import Controllers.ExpensesMonthlyController;
+import eapli.util.Console;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ *
+ * @author adao
+ */
+public class ExpensesMonthlyUI {
+        public void mainLoop() {
+        System.out.println("* * *  CONSULTA DE GASTOS MENSAIS  * * *\n");
+        
+        String what = Console.readLine("Introduza data (formato MM/AAAA):");
+        String[] dados = what.split("/");
+        
+        ExpensesMonthlyController controller = new ExpensesMonthlyController();
+        
+        controller.consultaDadosMensais(dados[0], dados[1]);
+        
+        System.out.println("So far, no expenses...");
+    }
+}
