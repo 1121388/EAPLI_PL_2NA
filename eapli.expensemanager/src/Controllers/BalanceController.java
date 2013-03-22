c8d07c42b3c7bdc712bb07abf829d64fe8c693d4
pseeno <pseeno@serenopc>
2013-03-22 23:37:42 +0000

    Visualizar Saldo v1.0

---
+++ eapli.expensemanager/src/Controllers/BalanceController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controllers;
+
+import Model.ChekingAccount;
+
+/**
+ *
+ * @author pseeno
+ */
+public class BalanceController {
+    
+    Model.ChekingAccount conta = new ChekingAccount ();
+  
+    
+    public void visualizarSaldo(){
+    
+    conta.getSaldo();
+    
+    }
+    
+    
+    
+}
