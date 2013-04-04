012b116a33cb13774738d1e4f57e8bf6a157de3d
i111051 <i111051@PCI124.dei.isep.ipp.pt>
2013-04-04 20:01:55 +0100

    + Adicionada a class ExpensesOfTheWeekController

---
+++ eapli.expensemanager/src/Controllers/ExpensesOfTheWeekController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controllers;
+
+import Model.Expense;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author i111051
+ */
+public class ExpensesOfTheWeekController {
+    
+    public BigDecimal currentlyWeekExpenses(ArrayList<Expense> despesas)
+    {
+        
+        return null;
+    }
+}
