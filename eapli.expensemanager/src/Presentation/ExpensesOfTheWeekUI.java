0d777c2c99ade3e6c0e7f994305bedb477cb0a5c
i111051 <i111051@PCI124.dei.isep.ipp.pt>
2013-04-04 20:13:00 +0100

    + Adicionada a class ExpensesOfTheWeekUI

---
+++ eapli.expensemanager/src/Presentation/ExpensesOfTheWeekUI.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Presentation;
+
+/**
+ *
+ * @author i111051
+ */
+public class ExpensesOfTheWeekUI {
+     
+    public void main() {
+        System.out.println("* * *  CONSULTA DO TOTAL DOS GASTOS SEMANAIS  * * *\n");
+    }
+}
