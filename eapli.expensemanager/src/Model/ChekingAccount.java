ec46e60f1f30007fb1bbcbaa8f16352fce415f17
Adao Santos <adaoss@gmail.com>
2013-04-11 19:19:31 +0100

    Alteração da função de Despesas mensais, para refletir nova UI

--- eapli.expensemanager/src/Model/ChekingAccount.java
+++ eapli.expensemanager/src/Model/ChekingAccount.java
@@ -4,6 +4,8 @@ import Persistence.BalanceRepository;
 import Persistence.ExpenseRepository;
 import Persistence.IncomeRepository;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class ChekingAccount {
@@ -59,5 +61,28 @@ public class ChekingAccount {
         for (int i = 0; i < receitas.size(); i++) {
             treceitas.add((receitas.get(i)).getAmount());
         }
+    }
+    
+    //--------- Método a ser usado pelas funções que necessitem de despesas por periodo de tempo.
+    public List<Expense> getExpensesByPeriod(Date start, Date end) {
+        List<Expense> _resultado = null;
+        int index = despesas.size();
+        
+        if ( index == 0)
+            return _resultado;
+        
+        for(;index>0;index--) {
+            if ( despesas.get(index).dateOccurred.after(start) && despesas.get(index).dateOccurred.before(end))
+                _resultado.add(despesas.get(index));
+        }
+        
+        return _resultado;
+     }
+    
+    //----------- Obter a lista de despesas, agrupadas por tipo, de um determinado mês
+    public List<Expense> getMonthlyExpenses(Date inicio, Date fim)  {
+          List<Expense> _resultado = this.getExpensesByPeriod(inicio, fim);
+         
+          return _resultado;
     }
 }
