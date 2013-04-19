6625945ae817ef0148a783b924d77e221096f401
MNLyle <MNLyle@WTFAYTA-FS>
2013-04-19 23:33:14 +0100

    Teste Expense

--- eapli.expensemanager/test/Model/ExpenseTest.java
+++ eapli.expensemanager/test/Model/ExpenseTest.java
@@ -4,26 +4,33 @@
  */
 package Model;
 
+import eapli.util.Console;
 import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
-import org.junit.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
  *
- * @author Ana
+ * @author MNLyle
  */
 public class ExpenseTest {
     
     public ExpenseTest() {
     }
-
+    
     @BeforeClass
-    public static void setUpClass() throws Exception {
+    public static void setUpClass() {
     }
-
+    
     @AfterClass
-    public static void tearDownClass() throws Exception {
+    public static void tearDownClass() {
     }
     
     @Before
@@ -38,41 +45,69 @@ public class ExpenseTest {
      * Test of getAmount method, of class Expense.
      */
     @Test
-    public void testGetAmount() {
+    public void testGetAmount() throws ParseException {
         System.out.println("getAmount");
-        Expense instance = null;
-        BigDecimal expResult = null;
+        SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+        Date date = df.parse("1-1-1");
+        ExpenseType et = new ExpenseType("TipoDespesa");
+        PaymentType pt = new PaymentType("DinheiroTipo");
+        MeansOfPayment mp = new MeansOfPayment(pt);
+        Expense instance = new Expense("Jantar", date, new BigDecimal(12), et, mp);
+        BigDecimal expResult = new BigDecimal(12);
         BigDecimal result = instance.getAmount();
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //fail("The test case is a prototype.");
     }
 
+    @Test
+    public void testGetAmount2() {
+        System.out.println("getAmount");
+        ExpenseType et = new ExpenseType("TipoDespesa");
+        PaymentType pt = new PaymentType("cheque");
+        MeansOfPayment mp = new MeansOfPayment(pt, 1234567890);
+        Expense instance = new Expense("Jantar", 1,1,1, new BigDecimal(12), et, mp);
+        BigDecimal expResult = new BigDecimal(12);
+        BigDecimal result = instance.getAmount();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+    
     /**
      * Test of getExpenseType method, of class Expense.
      */
     @Test
     public void testGetExpenseType() {
         System.out.println("getExpenseType");
-        Expense instance = null;
-        ExpenseType expResult = null;
+        ExpenseType et = new ExpenseType("TipoDespesa");
+        PaymentType pt = new PaymentType("cheque");
+        MeansOfPayment mp = new MeansOfPayment(pt, 1234567890);
+        Expense instance = new Expense("Jantar", 1,1,1, new BigDecimal(12), et, mp);
+        ExpenseType expResult = et;
         ExpenseType result = instance.getExpenseType();
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //fail("The test case is a prototype.");
     }
 
     /**
      * Test of getDateOccurred method, of class Expense.
      */
     @Test
-    public void testGetDateOccurred() {
+    public void testGetDateOccurred() throws ParseException {
         System.out.println("getDateOccurred");
-        Expense instance = null;
-        Date expResult = null;
+        SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+        Date date = df.parse("1-1-1");
+        ExpenseType et = new ExpenseType("TipoDespesa");
+        PaymentType pt = new PaymentType("DinheiroTipo");
+        MeansOfPayment mp = new MeansOfPayment(pt);
+        Expense instance = new Expense("Jantar", date, new BigDecimal(12), et, mp);
+        Date expResult = date;
         Date result = instance.getDateOccurred();
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //fail("The test case is a prototype.");
     }
 }
