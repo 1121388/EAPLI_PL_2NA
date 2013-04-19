7f5e04b7c3daae11c895a55c244e93b678ae02f4
Hugo Silva <hugoflsilva@gmail.com>
2013-04-19 23:02:15 +0100

    Criação das classes básicas dos Estabelecimento de poupança da EstablishmentOfSavingUI e EstablishmentOfSavingController

---
+++ eapli.expensemanager/src/Controllers/EstablishmentOfSavingController.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controllers;
+
+/**
+ *
+ * @author Hugo Silva
+ */
+public class EstablishmentOfSavingController extends BasicController {
+    
+}
