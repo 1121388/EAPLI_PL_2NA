3af562960a9333c68fd49639a836dfbad82a942c
Pc <Pc@Asus>
2013-05-02 20:05:00 +0100

    Grupo 9 - SavingGoalController

---
+++ eapli.expensemanager/src/Controllers/SavingGoalController.java
@@ -0,0 +1,19 @@
+package Controllers;
+
+import Model.SavingGoal;
+import java.util.Date;
+
+public class SavingGoalController extends BasicController {
+
+    public SavingGoalController() {
+    }
+
+    public SavingGoal CreateSavingGoal(String desc, Double valor, Date data, String alias) {
+        SavingGoal sav = new SavingGoal();
+        //sav.
+
+
+
+        return sav;
+    }
+}
