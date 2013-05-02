95bb5a6476280d4ed971dd5f17c305ea662e91ba
i980315 <i980315@PCI123.dei.isep.ipp.pt>
2013-05-02 20:08:44 +0100

    Alteração das classes IncomeType, IncomeTypeRegisterController, IncomeTypeRepository para suportar persistência de dados em memoria ou basa de dados...

---
+++ eapli.expensemanager/src/Controllers/LimitController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controllers;
+
+import Model.Expense;
+import Model.Limit;
+import Model.LimitType;
+import Persistence.LimitRepository;
+import Persistence.PersistenceFactory;
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author Hugo Silva
+ */
+public class LimitController extends BasicController {
+    
+    //LimitRepository repo = LimitRepository.GetInstance();
+    LimitRepository repo = PersistenceFactory.buildPersistenceFactory().LimitRepository();
+    
+    public LimitController() {
+    }
+    
+    public void creatLimit(Integer id, BigDecimal v1, BigDecimal v2) {
+        LimitType t = new LimitType(id, null);
+        Limit tmp = new Limit( t, v1, v2);
+        repo.save(tmp);
+    }
+}
