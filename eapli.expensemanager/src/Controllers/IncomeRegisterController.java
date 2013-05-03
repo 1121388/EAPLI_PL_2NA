0d33dc4e446ef857a4a6d168c64b62ba3d9fc68d
vcosta46 <1111169@isep.ipp.pt>
2013-05-03 22:44:31 +0100

    Retirado o GetInstance() e alterado para uma PersistenceFactory

--- eapli.expensemanager/src/Controllers/IncomeRegisterController.java
+++ eapli.expensemanager/src/Controllers/IncomeRegisterController.java
@@ -14,25 +14,28 @@ import java.util.List;
  *
  * @author jfoliveira
  */
-public class IncomeRegisterController extends BasicController{
-    
-        
-    IncomeRepository incomeRepository = IncomeRepository.GetInstance();
-    IncomeTypeRepository incomeTypeRepository = IncomeTypeRepository.getInstance();
-    
-    public List<String> IncomeTypeList(){
-        if(IncomeTypeList().isEmpty()){
+public class IncomeRegisterController extends BasicController {
+
+//    IncomeRepository incomeRepository = IncomeRepository.GetInstance();
+//    IncomeTypeRepository incomeTypeRepository = IncomeTypeRepository.getInstance();
+    IncomeTypeRepository incomeTypeRepository = PersistenceFactory.buildPersistenceFactory().incomeTypeRepository();
+
+    public List<String> IncomeTypeList() {
+        if (IncomeTypeList().isEmpty()) {
             System.out.println("You must define first a type of income");
         }
         return incomeTypeRepository.getIncomeTypesStrings();
-        
+
     }
+
+    public IncomeRegisterController() {
+    }
+
+//    public IncomeType GetIncomeType(int incomeType){
+//        return incomeTypeRepository.GetIncomeType(incomeType);
+//    }
+//    
     public void registerIncome(String what, Date date, BigDecimal amount, IncomeType incomeType) {
-        new ChekingAccount().registerIncome( what, date, amount,incomeType);
-         
-        //Income income = new Income( what, date, amount, incomeType);
-        //incomeRepository.save(income);
+        new ChekingAccount().registerIncome(what, date, amount, incomeType);
     }
-    
-    
 }
