3751d153085d9684b6fe812ecbffb3a15f5669ad
SofiaLoboLima <SofiaLoboLima@sophiie>
2013-05-16 19:30:12 +0100

    utilização de BigDecimal recepção de BigDecimal da consola

--- eapli.expensemanager/src/Persistence/PersistenceFactory.java
+++ eapli.expensemanager/src/Persistence/PersistenceFactory.java
@@ -4,22 +4,50 @@
  */
 package Persistence;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.lang.reflect.*;
+import java.util.Scanner;
 
-/**
- *
- * @author arocha
- */
 public class PersistenceFactory {
-    
+
     private PersistenceFactory() {
     }
 
     public static RepositoryFactory buildPersistenceFactory() {
-    JpaRepositoryFactory repo = new JpaRepositoryFactory();
-     //InMemoryRepositoryFactory repo= new InMemoryRepositoryFactory();
-        return (RepositoryFactory)  repo;
-  
+
+        Object repo = null;
+
+        try {
+            String name = searchRepo();
+            Class clazz = Class.forName(name);
+            repo = clazz.newInstance();
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("Error:Config File not found!");
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Error:Class do not exist!");
+        } catch (InstantiationException ex) {
+            System.out.println("Error: Instantiation attempted failed");
+        } catch (IllegalAccessException ex) {
+            System.out.println("Error:IllegalAccess");
+        }
+        return (RepositoryFactory) repo;
+    }
+
+    private static String searchRepo() throws FileNotFoundException {
+
+        Scanner file = new Scanner(new File("config.txt"));
+        String aux = "noconfig";
+        while ((aux = file.nextLine()) != null) {
+            String[] linhas = aux.split(":");
+            if (linhas[0].equalsIgnoreCase("BD")) {
+                aux = linhas[1];
+            }
+        }
+        file.close();
+        return aux;
     }
 }
